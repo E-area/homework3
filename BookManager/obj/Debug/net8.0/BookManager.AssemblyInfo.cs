725db1b9f1d647b2994a18a687ea3a4f5a340f32
Terra2333 <2630906931@qq.com>
2024-10-25 19:09:48 +0800

    added some useless things

--- BookManager/obj/Debug/net8.0/BookManager.AssemblyInfo.cs
+++ BookManager/obj/Debug/net8.0/BookManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f54b849cbb1a48b48f87b6cee7db2342df39467")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
